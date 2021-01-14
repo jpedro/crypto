86b7cd2da3c67e7b4431ee96eb49b32d4e3ee31d
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-01-14 14:52:48 +0100

    Another bug bites the dust

---
+++ example/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+    "fmt"
+
+    "github.com/jpedro/crypto"
+)
+
+func main() {
+    text := "test"
+    pass := "test"
+
+	encryted, _ := crypto.Encrypt(text, pass)
+	fmt.Println(encryted)
+}
