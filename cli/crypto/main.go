b92940f666a18cd2dcb259562fa7348ac24a8b63
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-01-15 13:25:08 +0100

    One little whitespace gets its very own commit! Oh, life is so erratic!

--- cli/crypto/main.go
+++ cli/crypto/main.go
@@ -1,3 +1,12 @@
+// Swine leberkas venison
+//
+// Burgdoggen sirloin biltong chuck drumstick shank capicola porchetta. Turkey pork loin
+// chuck fatback jowl. T-bone short ribs turducken cupim, brisket cow pork belly leberkas.
+// Landjaeger ham hock fatback pig corned beef bresaola beef ribs. Pork pork chop boudin
+// strip steak landjaeger, pork belly kevin pork loin capicola ham. Pastrami spare ribs
+// porchetta, drumstick leberkas t-bone short loin doner filet mignon hamburger corned
+// beef. Venison short loin flank, cupim fatback spare ribs pork loin buffalo turducken
+// tail.package main
 package main
 
 import (
